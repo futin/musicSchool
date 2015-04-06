6e68779d86d710d025d711ade5d13d40c21816bc
Andreja Jevtic <andrajevtic@gmail.com>
2015-04-06 21:15:09 +0200

    again error with clean project, and deleting all images. Its all good now

--- app/src/main/java/com/example/futin/tabletest/userInterface/login/Welcome.java
+++ app/src/main/java/com/example/futin/tabletest/userInterface/login/Welcome.java
@@ -3,6 +3,8 @@ package com.example.futin.tabletest.userInterface.login;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -12,11 +14,12 @@ import com.example.futin.tabletest.userInterface.mainPage.MainPage;
 import android.os.Handler;
 import android.util.Log;
 import android.view.Window;
+import android.widget.TextView;
 
 
 public class Welcome extends ActionBarActivity {
 
-    //TODO make it on 2000
+    TextView txtIpAddress;
     private final int DELAYED_TIME = 2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,8 +41,31 @@ public class Welcome extends ActionBarActivity {
 
             }
         },DELAYED_TIME );
+/*
+        //return ip address
 
+        WifiManager myWifiManager = (WifiManager)getSystemService(Context.WIFI_SERVICE);
 
+        WifiInfo myWifiInfo = myWifiManager.getConnectionInfo();
+        int myIp = myWifiInfo.getIpAddress();
+
+        int intMyIp3 = myIp/0x1000000;
+        int intMyIp3mod = myIp%0x1000000;
+
+        int intMyIp2 = intMyIp3mod/0x10000;
+        int intMyIp2mod = intMyIp3mod%0x10000;
+
+        int intMyIp1 = intMyIp2mod/0x100;
+        int intMyIp0 = intMyIp2mod%0x100;
+        txtIpAddress=new TextView(this);
+        txtIpAddress.setText(String.valueOf(intMyIp0)
+                        + "." + String.valueOf(intMyIp1)
+                        + "." + String.valueOf(intMyIp2)
+                        + "." + String.valueOf(intMyIp3)
+        );
+
+        Log.i("",txtIpAddress.getText().toString());
+        */
     }
 
 
